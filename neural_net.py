73294cd9e739dfb3330eecb60352e55f6f74cdc8
Vernon Foley <vfoley@mtech.edu>
2017-11-09 00:34:43 -0700

    Current status weighted input returned and printed out. w/ BIAS_VALUE of 1 added in to avoid 0 values after weighting.

--- neural_net.py
+++ neural_net.py
@@ -7,7 +7,7 @@ import random
 # the last value in each entry is the expected or correct outcome
 normalized_weather = weather_array()
 normalized_class = class_array()
-
+BIAS_VALUE = 1
 
 #returns layer of weights ranomized -.5 <= w <= .5
 def randomize_node_weights(num):
@@ -16,6 +16,18 @@ def randomize_node_weights(num):
         node.append(random.uniform(-.5,.5))
     
     return node
+
+#returns entry passed through node
+def return_weighted_input(node=[],input=[]):
+    weighted_value = 0
+    weighted_input = []
+    for i in range(0,len(node)):
+        weighted_value += BIAS_VALUE
+        for j in range(0,len(node)):
+            #summation of weights to each node.
+            weighted_value += (node[i]*input[j])
+        weighted_input.append(weighted_value)
+    return weighted_input
 # start the process of training 
 # each layer represented by an array holding weights
 # to be applied to the data
@@ -24,10 +36,16 @@ def randomize_node_weights(num):
 # evaluate the accuracy of the training
 
 layers = []
+transition_array = []
 layers.append(randomize_node_weights(4))
 
-for line in layers:
+for line in normalized_weather:
+    for node in layers:
+        transition_array.append(return_weighted_input(node, line))
+
+for line in transition_array:
     print(line)
+            
 
 
 #end
